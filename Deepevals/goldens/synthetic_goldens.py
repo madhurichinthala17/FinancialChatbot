dc3275f6ade14391eeefd89cee50f8dcc95c380e
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-05-12 20:01:52 -0500

    init goldens

--- Deepevals/goldens/synthetic_goldens.py
+++ Deepevals/goldens/synthetic_goldens.py
@@ -1,32 +1,25 @@
 from deepeval.synthesizer import Synthesizer
 from deepeval.synthesizer.config import ContextConstructionConfig
 from deepeval.models import OllamaModel
-from deepeval.dataset import EvaluationDataset, Golden
+from deepeval.dataset import EvaluationDataset
 
-PDF_PATH= "C:\\Users\\madhu\\FinancialChatbot-ChainBased\\testdata\\JPmorgan10kReport.pdf"
+PDF_PATH = "C:\\Users\\madhu\\FinancialChatbot-ChainBased\\testdata\\JPmorgan10kReport.pdf"
 
-#For Document Parsing and Context grouping
-embeddingmodel = OllamaModel(model = "nomic-embed-text:latest")
-
-#To determine Context
-retrievermodel = OllamaModel(model= "deepseek-r1:8b")
-
+retrievermodel = OllamaModel(model="deepseek-r1:8b")
 
 synthesizer = Synthesizer()
 
 goldens = synthesizer.generate_goldens_from_docs(
-    document_paths= PDF_PATH,
+    document_paths=[PDF_PATH],
     include_expected_output=True,
     max_goldens_per_context=3,
     context_construction_config=ContextConstructionConfig(
-        critic_model= retrievermodel,
+        critic_model=retrievermodel,
         chunk_size=1200,
         chunk_overlap=500,
-        max_retries =1,
-        embedder = embeddingmodel
+        max_retries=1
     )
 )
 
-dataset=EvaluationDataset()
-
+dataset = EvaluationDataset(goldens=goldens)
 dataset.push("Synthetic Golden from Doc")
